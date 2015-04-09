f8e567f0b270a78dbd23f353dce823bff6a2080c
Aila <zee2198@gmail.com>
2015-04-09 14:53:48 -0400

    Add Contact

--- crm.rb
+++ crm.rb
@@ -1,6 +1,24 @@
+require_relative 'contact'
+require_relative 'rolodex'
 require 'sinatra'
+
+#global var./initiate/access from each section
+$rolodex= Rolodex.new
 
 get '/' do
   @crm_app_name = "My CRM"
 	erb :index
+end
+
+get '/contacts/new' do
+  erb :new_contact
+end
+
+get '/contacts' do
+
+  erb :contacts
+end
+
+get '/add_contact' do
+  erb :add_contact
 end
